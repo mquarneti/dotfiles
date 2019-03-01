c625688444201c05c0996c3bc62b0c8b2ca6ade7
Manuel Quarneti <manuelquarneti@protonmail.com>
2019-03-01 08:03:50 +0100

    Fix nvim installation

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+
 ln -sf ~/.dotfiles/.config/chromium-flags.conf ~/.config/chromium-flags.conf
+
+mkdir ~/.config/nvim
 ln -sf ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
