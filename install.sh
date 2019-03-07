880b3f3ed172950568c04709c09058f755e47378
Manuel Quarneti <manuelquarneti@protonmail.com>
2019-03-07 18:06:36 +0100

    Added chromium policy. tidying up

--- install.sh
+++ install.sh
@@ -2,8 +2,12 @@
 
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 
-mkdir ~/.config
-ln -sf ~/.dotfiles/.config/chromium-flags.conf ~/.config/chromium-flags.conf
+mkdir -p ~/.config/nvim
+ln -sf ~/.dotfiles/init.vim ~/.config/nvim/init.vim
 
-mkdir ~/.config/nvim
-ln -sf ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/.dotfiles/chromium/chromium-flags.conf ~/.config/chromium-flags.conf
+
+sudo mkdir -p /etc/chromium/policies/managed
+sudo chown root ~/.dotfiles/chromium/policy.json
+sudo ln -sf ~/.dotfiles/chromium/policy.json /etc/chromium/policies/managed/policy.json
+
