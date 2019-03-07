362847e5e1a2e4d74b18cebe48f086c653708e67
Manuel Quarneti <manuelquarneti@protonmail.com>
2019-03-07 18:57:02 +0100

    deleted chromium policy. switch to vim from neovim

--- install.sh
+++ install.sh
@@ -2,12 +2,7 @@
 
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 
-mkdir -p ~/.config/nvim
-ln -sf ~/.dotfiles/init.vim ~/.config/nvim/init.vim
+ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 
-ln -sf ~/.dotfiles/chromium/chromium-flags.conf ~/.config/chromium-flags.conf
-
-sudo mkdir -p /etc/chromium/policies/managed
-sudo chown root ~/.dotfiles/chromium/policy.json
-sudo ln -sf ~/.dotfiles/chromium/policy.json /etc/chromium/policies/managed/policy.json
-
+mkdir ~/.config
+ln -sf ~/.dotfiles/.config/chromium-flags.conf ~/.config/chromium-flags.conf
