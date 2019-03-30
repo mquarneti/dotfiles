b88a81457180c98101ffea02c85f3eeb9215297d
Manuel Quarneti <manuelquarneti@protonmail.com>
2019-03-30 16:19:20 +0100

    commented chromium-flags.conf in installer

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ ln -sf ~/.dotfiles/.zshrc ~
 mkdir -p ~/.config/nvim
 ln -sf ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim
 
-ln -sf ~/.dotfiles/.config/chromium-flags.conf ~/.config/chromium-flags.conf
+#ln -sf ~/.dotfiles/.config/chromium-flags.conf ~/.config/chromium-flags.conf
