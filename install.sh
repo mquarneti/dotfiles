d217ab7a1373503ce673acbe531c8b3518591166
Manuel Quarneti <manuelquarneti@protonmail.com>
2019-03-30 16:57:25 +0100

    add mpv.conf

--- install.sh
+++ install.sh
@@ -5,6 +5,15 @@ ln -sf ~/.dotfiles/.zshrc ~
 mkdir -p ~/.config/nvim
 ln -sf ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim
 
+# set the correct hwdec method
+case $PRESET in
+    desktop) echo 'hwdec=nvdec' >> ~/.dotfiles/.config/mpv/mpv.conf
+    laptop)  echo 'hwdec=vaapi' >> ~/.dotfiles/.config/mpv/mpv.conf
+esac
+
+mkdir -p ~/.config/mpv
+ln -sf ~/.dotfiles/.config/mpv/mpv.conf ~/.config/mpv
+
 if [ "$WEB_BROWSER" = "chromium" ]; then
     ln -sf ~/.dotfiles/.config/chromium-flags.conf ~/.config/chromium-flags.conf
 fi
