33a141e8dc61fa8ed68042c3037d41b08552b27c
Manuel Quarneti <manuelquarneti@protonmail.com>
2019-03-30 16:36:18 +0100

    install chromium-flags.conf only if chromium is the preferred web browser

--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ ln -sf ~/.dotfiles/.zshrc ~
 mkdir -p ~/.config/nvim
 ln -sf ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim
 
-#ln -sf ~/.dotfiles/.config/chromium-flags.conf ~/.config/chromium-flags.conf
+if [ "$WEB_BROWSER" = "chromium" ]; then
+    ln -sf ~/.dotfiles/.config/chromium-flags.conf ~/.config/chromium-flags.conf
+fi
