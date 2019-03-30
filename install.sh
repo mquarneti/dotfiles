09170b1e9f90b431c32a945bfdc59a96f345582e
Manuel Quarneti <manuelquarneti@protonmail.com>
2019-03-30 21:40:14 +0100

    added code

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/bash
 
+# zsh
 ln -sf ~/.dotfiles/.zshrc ~
 
+# neovim
 mkdir -p ~/.config/nvim
 ln -sf ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim
+
+# mpv
 
 # set the correct hwdec method
 case $PRESET in
@@ -14,6 +18,13 @@ esac
 mkdir -p ~/.config/mpv
 ln -sf ~/.dotfiles/.config/mpv/mpv.conf ~/.config/mpv
 
-if [ "$WEB_BROWSER" = "chromium" ]; then
-    ln -sf ~/.dotfiles/.config/chromium-flags.conf ~/.config/chromium-flags.conf
+# chromium
+if $INSTALL_CHROMIUM; then
+    ln -sf ~/.dotfiles/.config/chromium-flags.conf ~/.config
+fi
+
+# code (visual studio code OSS build)
+if $INSTALL_CODE; then
+    mkdir -p '~/.config/Code - OSS'
+    ln -sf '~/.dotfiles/.config/Code - OSS/settings.json' '~/.config/Code - OSS'
 fi
