111730c0e069255b4c5bc3eeb7e5653ee36a5c1e
Manuel Quarneti <manuelquarneti@protonmail.com>
2019-05-13 18:13:40 +0200

    removed code config, maybe mpv.conf works this time

--- install.sh
+++ install.sh
@@ -11,8 +11,10 @@ ln -sf ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim
 
 # set the correct hwdec method
 case $PRESET in
-    desktop) echo "\nhwdec=nvdec" >> ~/.dotfiles/.config/mpv/mpv.conf;;
-    laptop)  echo "\nhwdec=vaapi" >> ~/.dotfiles/.config/mpv/mpv.conf;;
+    desktop) echo "
+hwdec=nvdec" >> ~/.dotfiles/.config/mpv/mpv.conf;;
+    laptop)  echo "
+hwdec=vaapi" >> ~/.dotfiles/.config/mpv/mpv.conf;;
 esac
 
 mkdir -p ~/.config/mpv
@@ -21,10 +23,4 @@ ln -sf ~/.dotfiles/.config/mpv/mpv.conf ~/.config/mpv
 # chromium
 if $INSTALL_CHROMIUM; then
     ln -sf ~/.dotfiles/.config/chromium-flags.conf ~/.config
-fi
-
-# code (visual studio code OSS build)
-if $INSTALL_CODE; then
-    mkdir -p ~/.config/Code\ -\ OSS
-    ln -sf ~/.dotfiles/.config/Code\ -\ OSS/settings.json ~/.config/Code\ -\ OSS
 fi
