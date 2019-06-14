fe4523d51a8d2aaede1c444f343e98e88e356308
Manuel Quarneti <manuelquarneti@protonmail.com>
2019-06-14 14:36:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,19 +8,10 @@ mkdir -p ~/.config/nvim
 ln -sf ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim
 
 # mpv
-
-# set the correct hwdec method
-case $PRESET in
-    desktop) echo "
-hwdec=nvdec" >> ~/.dotfiles/.config/mpv/mpv.conf;;
-    laptop)  echo "
-hwdec=vaapi" >> ~/.dotfiles/.config/mpv/mpv.conf;;
-esac
-
 mkdir -p ~/.config/mpv
 ln -sf ~/.dotfiles/.config/mpv/mpv.conf ~/.config/mpv
 
 # chromium
-if $INSTALL_CHROMIUM; then
+if [ $(which chromium 2> /dev/null) ]; then
     ln -sf ~/.dotfiles/.config/chromium-flags.conf ~/.config
 fi
