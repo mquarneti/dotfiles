5f8c79a46e2943f94f9c368d76b864b2c8333559
Manuel Quarneti <manuelquarneti@protonmail.com>
2019-08-09 23:26:06 +0200

    Prefer regular oh-my-zsh config

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # zsh
-ln -sf ~/.dotfiles/.zshrc ~
+[ ! -f ~/.zshrc ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # neovim
 mkdir -p ~/.config/nvim
