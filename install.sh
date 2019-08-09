b1acb1d36d2b74546854d8beb53e78290553a18e
Manuel Quarneti <manuelquarneti@protonmail.com>
2019-08-09 23:29:48 +0200

    Fix oh-my-zsh install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # zsh
-[ ! -f ~/.zshrc ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+[ ! -f ~/.zshrc ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 
 # neovim
 mkdir -p ~/.config/nvim
