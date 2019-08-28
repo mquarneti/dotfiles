ba0c2624b36aabfa64e7361787ec799d2df766e5
Manuel Quarneti <manuelquarneti@protonmail.com>
2019-08-28 17:33:58 +0200

    Install configs only if applications are installed

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/bash
 
 # zsh
-[ ! -f ~/.zshrc ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+if [ $(which zsh 2> /dev/null) ]; then
+    [ ! -f ~/.zshrc ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+fi
 
 # neovim
-mkdir -p ~/.config/nvim
-ln -sf ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim
+if [ $(which nvim 2> /dev/null) ]; then
+    mkdir -p ~/.config/nvim
+    ln -sf ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim
+fi
 
 # mpv
-mkdir -p ~/.config/mpv
-ln -sf ~/.dotfiles/.config/mpv/mpv.conf ~/.config/mpv
+if [ $(which mpv 2> /dev/null) ]; then
+    mkdir -p ~/.config/mpv
+    ln -sf ~/.dotfiles/.config/mpv/mpv.conf ~/.config/mpv
+fi
 
 # chromium
 if [ $(which chromium 2> /dev/null) ]; then
