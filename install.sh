4caae0aed0c69ad8081d07628653ad17a54ca25f
Manuel Quarneti <manuelquarneti@protonmail.com>
2019-09-01 16:22:11 +0200

    https://github.com/dylanaraps/pure-bash-bible#check-if-a-program-is-in-the-users-path

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 # zsh
-if [ $(which zsh 2> /dev/null) ]; then
+if type -p zsh &>/dev/null; then
     [ ! -f ~/.zshrc ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 fi
 
 # neovim
-if [ $(which nvim 2> /dev/null) ]; then
+if type -p nvim &>/dev/null; then
     mkdir -p ~/.config/nvim
     ln -sf ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim
 fi
 
 # mpv
-if [ $(which mpv 2> /dev/null) ]; then
+if type -p mpv &>/dev/null; then
     mkdir -p ~/.config/mpv
     ln -sf ~/.dotfiles/.config/mpv/mpv.conf ~/.config/mpv
 fi
 
 # chromium
-if [ $(which chromium 2> /dev/null) ]; then
+if type -p chromium &>/dev/null; then
     ln -sf ~/.dotfiles/.config/chromium-flags.conf ~/.config
 fi
