6efcb482957c9294a1a0a78d5b4da56ca09d04c4
Manuel Quarneti <manuelquarneti@gmail.com>
2019-11-17 16:35:08 +0100

    The dotfiles are now variables on install.sh

---
+++ README.md
@@ -0,0 +1,9 @@
+# dotfiles
+
+This script is intended to work on my devices (desktop and laptop), feel free to take a look
+
+## Usage
+
+```sh
+bash -c "$(curl -sSL git.io/Jeo5h)"
+```
--- install.sh
+++ install.sh
@@ -1,5 +1,37 @@
 #!/usr/bin/env bash
 
+INIT_VIM="\
+\" Relative line numbers on the left
+set relativenumber
+
+\" Default register is the system clipboard
+set clipboard=unnamedplus
+"
+
+MPV_CONF="\
+profile=gpu-hq
+gpu-api=vulkan
+
+scale=ewa_lanczossharp
+dscale=ewa_lanczossharp
+cscale=ewa_lanczossoft
+
+keep-open=yes
+
+[extension.gif]
+loop-file=yes
+"
+
+CHROMIUM_FLAGS_CONF="\
+--enable-accelerated-mjpeg-decode
+--enable-accelerated-video
+--ignore-gpu-blacklist
+--enable-gpu-rasterization
+--enable-native-gpu-memory-buffers
+--enable-zero-copy
+--disable-gpu-driver-bug-workarounds
+"
+
 # zsh
 if type -p zsh &>/dev/null; then
     [ ! -f ~/.zshrc ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
@@ -8,16 +40,16 @@ fi
 # neovim
 if type -p nvim &>/dev/null; then
     mkdir -p ~/.config/nvim
-    ln -sf ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim
+    echo "$INIT_VIM" > ~/.config/nvim/init.vim
 fi
 
 # mpv
 if type -p mpv &>/dev/null; then
     mkdir -p ~/.config/mpv
-    ln -sf ~/.dotfiles/.config/mpv/mpv.conf ~/.config/mpv
+    echo "$MPV_CONF" ~/.config/mpv/mpv.conf
 fi
 
 # chromium
 if type -p chromium &>/dev/null; then
-    ln -sf ~/.dotfiles/.config/chromium-flags.conf ~/.config
+    echo "$CHROMIUM_FLAGS_CONF" ~/.config/chromium-flags.conf
 fi
